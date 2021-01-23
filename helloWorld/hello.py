45cdba0ee41d3eb0b6629df8c78f4217c9b98f0f
Daniel Bae <danieljbaee@hotmail.com>
2021-01-22 16:46:36 -0800

    added basic functions

--- helloWorld/hello.py
+++ helloWorld/hello.py
@@ -22,3 +22,22 @@ class MovingAround(Scene):
         self.play(square.animate.set_fill(ORANGE))
         self.play(square.animate.scale(0.3))
         self.play(square.animate.rotate(0.4))
+
+
+class MovingFrameBox(Scene):
+    def construct(self):
+        text = MathTex(
+            "\\frac{d}{dx}f(x)g(x)=", "f(x)\\frac{d}{dx}g(x)", "+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff=.1)
+        framebox2 = SurroundingRectangle(text[3], buff=.1)
+        self.play(
+            ShowCreation(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1, framebox2),
+        )
+        self.wait()
